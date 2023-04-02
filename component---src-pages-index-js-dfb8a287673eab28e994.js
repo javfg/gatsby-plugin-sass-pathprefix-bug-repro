d268528c8ab82f32a499f73f6998b04f77ebf604
Javier Ferrer <javier.f.g@um.es>
2023-04-02 18:07:27 +0200

    Updates

---
+++ component---src-pages-index-js-dfb8a287673eab28e994.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_minimal=self.webpackChunkgatsby_starter_minimal||[]).push([[678],{6558:function(e,t,a){a.r(t),a.d(t,{Head:function(){return l}});var n=a(7294);t.default=()=>n.createElement("h1",null,"This header should have 'Pacifico' font-family.");const l=()=>n.createElement("title",null,"Home Page")}}]);
+//# sourceMappingURL=component---src-pages-index-js-dfb8a287673eab28e994.js.map
